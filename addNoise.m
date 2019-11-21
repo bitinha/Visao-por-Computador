b9c11af3e133ac9c42579202d6b7e7eb9d198b43
bitinha <vitordagomes@outlook.com>
2019-11-21 15:28:23 +0000

    Arranjei a addNoise

--- addNoise.m
+++ addNoise.m
@@ -3,11 +3,11 @@ function [imgOut] = addNoise(img,noiseType,varargin)
 %   Devolve uma imagem a partir da original com ruído gaussiano ou ruído
 %   sal e pimenta
 if strcmp('gaussian',noiseType);
-    imgOut = imnoise(img,'gaussian',varargin(1),varargin(2));
+    imgOut = imnoise(img,'gaussian',varargin{1},varargin{2});
 end
 
 if strcmp('salt & pepper',noiseType);
-    imgOut = imnoise(img,'salt & pepper',varargin(1));
+    imgOut = imnoise(img,'salt & pepper',varargin{1});
 end
 end
 
