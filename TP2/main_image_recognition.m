25c3ebb8c53c05e93ddc58925eafd3a15a7be689
bitinha <vitordagomes@outlook.com>
2020-01-02 09:26:56 +0000

    Reestruturação do código do main

--- TP2/main_image_recognition.m
+++ TP2/main_image_recognition.m
@@ -2,28 +2,30 @@ function [centers] = main_image_recognition(img_path,noise,noiseParameters)
 
 img = imread(img_path);
 
-%filter = fspecial('average',20);
+if strcmp(noise, 'gaussian')
+    img = addNoise(img,noise,noiseParameters(1),noiseParameters(2));
+else
+    img = addNoise(img,noise,noiseParameters(1));
+end
+
+%filter = fspecial('average',3);
 %img = imfilter(img,filter);
 
 %medfilt2(img,[5,5]);
 
 img = imgaussfilt(img,5,'FilterSize',11,'FilterDomain','spatial');
 
-%%img = histeq(img);
-%if strcmp(noise, 'gaussian')
-%    noisyImage = addNoise(img,noise,noiseParameters(1),noiseParameters(2));
-%else
-%    noisyImage = addNoise(img,noise,noiseParameters(1));
-%end
+img = histeq(img);
 
 can = edge(img,'Canny');
 
 [centers, radii, metric] = imfindcircles(can,[200 300],'Sensitivity',0.97);
-figure(1);
-imshow(img);
-viscircles(centers, radii,'EdgeColor','b');
+
+subplot(2,2,1), imshow(img);
+
+subplot(2,2,2),viscircles(centers, radii,'EdgeColor','b');
     
-figure(2);
-imshow(can);
+subplot(2,2,3),imshow(can);
+
 end
 
